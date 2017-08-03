943afd40b5173c4b89b799aa29005bfedbece0f9
Mark <mark.chmilar@ericsson.com>
2017-08-03 10:30:13 -0400

    hackerrank: arrays left rotation solution

---
+++ src/hackerrank/ArraysLeftRotation.java
@@ -0,0 +1,26 @@
+package hackerrank;
+
+import java.util.Scanner;
+
+/**
+ * https://www.hackerrank.com/challenges/ctci-array-left-rotation
+ * @author Mark Chmilar
+ *
+ */
+public class ArraysLeftRotation {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+        int l = in.nextInt();
+        int d = in.nextInt();
+        int a[] = new int[l];
+        for(int a_i=0; a_i < l; a_i++){
+            int newIndex = (a_i + l - (d % l)) % l;
+            a[newIndex] = in.nextInt();
+        }
+        for (int i = 0; i < l; i++) {
+            System.out.print(a[i] + ((i == l - 1) ? "" : " "));
+        }
+	}
+
+}
