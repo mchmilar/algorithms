ef08ec7af3e09f128805b94dfb163d7dbdf06d44
mchmilar <markchmilar@gmail.com>
2017-08-05 10:51:50 -0400

    hackerrank: solution- Quicksort Partition

---
+++ src/hackerrank/QuicksortPartition.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+
+/**
+ * https://www.hackerrank.com/challenges/quicksort1
+ * @author Mark Chmilar
+ */
+public class QuicksortPartition {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int pivot = in.nextInt();
+        ArrayList<Integer> left = new ArrayList<>();
+        ArrayList<Integer> right = new ArrayList<>();
+        for (int i = 1; i < n; i++) {
+            int number = in.nextInt();
+            if (number < pivot)
+                left.add(number);
+            else 
+                right.add(number);
+        }
+        left.add(pivot);
+        left.addAll(right);
+        for (Integer number : left)
+            System.out.print(number + " ");
+        in.close();
+    }
+}
