aa7ba32ecc536dad4e36a02e7c159523d70d8e51
Mark <mark.chmilar@ericsson.com>
2017-08-08 15:18:19 -0400

    begin suffix array solution

--- src/hackerrank/algorithms/strings/StringFunctionCalculation.java
+++ src/hackerrank/algorithms/strings/StringFunctionCalculation.java
@@ -1,26 +1,44 @@
 package hackerrank.algorithms.strings;
 
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class StringFunctionCalculation {
     static int maxValue(String t) {
+        String[] suffixes = suffix(t);
         int result = 0;
         for (int i = 0; i < t.length(); i++) {
             for (int j = i + 1; j < t.length(); j++) {
                 String ss = t.substring(i, j);
-                int count = 0;
-                for (int k = 0; k <= (t.length() - ss.length()); k++) {
-                    if (t.indexOf(ss, k) != -1)
-                        count++;
-                }
-                int score = count * (ss.length());
-                if (score > result)
-                    result = score;
+                int count = search(ss, suffixes);
+                if (count * ss.length() > result)
+                    result = count * ss.length();
             }
         }
         return result;
+        
+    }
+    
+    static int search(String s, String[] suffixes) {
+        int l = 0, r = s.length();
+        while (l < r) {
+            int mid = (l + r) / 2;
+            if (s.compareTo(suffixes[mid]) > 0)
+                r = mid;
+        }
+    }
+    
+    
+    
+    static String[] suffix(String t) {
+        String[] s = new String[t.length()];
+        for (int i = 0; i < t.length(); i++) {
+            s[i] = t.substring(i);
+        }
+        Arrays.sort(s);
+        return s;
     }
 
     public static void main(String[] args) {
